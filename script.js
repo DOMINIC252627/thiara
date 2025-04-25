df5cbeeff76c2f2cb602bd9998e824e9a6d2361e
DOMINIC252627 <152129613+DOMINIC252627@users.noreply.github.com>
2025-04-25 10:12:09 -0400

    ineiti comint

---
+++ script.js
@@ -0,0 +1,9 @@
+setTimeout(cambiaMensaje,2300)
+
+function cambiaMensaje() {
+document.getElementById("nikitam").innerText="Enviado!"
+}
+
+setTimeout(function() {
+ alert("Recibiste un abrazo")   
+},2400)
